varigit/meta-variscite-bsp-imx

commit 507e8d81cf393ac42d11b70b17aa1e77a017694c

imx-atf: Use the varigit only on variscite machines
Signed-off-by: Khem Raj <raj.khem@gmail.com>

recipes-bsp/imx-atf/imx-atf_2.6.bbappend
@@ -1,10 +1,10 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
-SRC_URI = "git://github.com/varigit/imx-atf;protocol=https;branch=${SRCBRANCH}"
-SRCBRANCH = "lf_v2.6_var03"
-SRCREV = "f21a37b137fd9e85d03cf44f2c2540356dd52fd9"
+SRC_URI:var-som = "git://github.com/varigit/imx-atf;protocol=https;branch=${SRCBRANCH}"
+SRCBRANCH:var-som = "lf_v2.6_var03"
+SRCREV:var-som = "f21a37b137fd9e85d03cf44f2c2540356dd52fd9"
 
-SRC_URI:mx8-nxp-bsp = "\
+SRCBRANCH:mx8-nxp-bsp:var-som = "\
            file://0001-Makefile-Suppress-array-bounds-error.patch \
            file://rwx-segments.patch \
 "
@@ -17,4 +17,4 @@ EXTRA_OEMAKE:append:imx8mm-var-dart = " \
     BL32_BASE=${TEE_LOAD_ADDR} \
 "
 
-COMPATIBLE_MACHINE = "(mx9-nxp-bsp)"
+COMPATIBLE_MACHINE:var-som = "(mx9-nxp-bsp)"

commit 5f7bc7031c40e81b3bb164e6dda14d27fa3c016d

imx-atf: Drop 2 patches as merged to NXP fork
Fixes: 507e8d81cf393ac42d11b70b17aa1e77a017694c
Derived from: https://github.com/nxp-imx/meta-imx/commit/b391a9171239a8ffa9ef9a9e5cf29ad98705ed44

Signed-off-by: Nate Drude <nate.d@variscite.com>

recipes-bsp/imx-atf/imx-atf_2.6.bbappend
@@ -4,11 +4,6 @@ SRC_URI:var-som = "git://github.com/varigit/imx-atf;protocol=https;branch=${SRCB
 SRCBRANCH:var-som = "lf_v2.6_var03"
 SRCREV:var-som = "f21a37b137fd9e85d03cf44f2c2540356dd52fd9"
 
-SRCBRANCH:mx8-nxp-bsp:var-som = "\
-           file://0001-Makefile-Suppress-array-bounds-error.patch \
-           file://rwx-segments.patch \
-"
-
 EXTRA_OEMAKE:append:imx8mq-var-dart = " \
     BL32_BASE=${TEE_LOAD_ADDR} \
 "