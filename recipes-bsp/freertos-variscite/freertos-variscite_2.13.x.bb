varigit/meta-variscite-bsp-imx

commit e1f8e21ee0e86ed27000b9b549e08e9ba87eb694

freertos-variscite: Update 2.13 to point to (now) upstream source
This was previously set to a private source as NXP code was not yet
public and could only be used if self-hosted by customer. The MX9
requires its own SRC_URI since there is not yet a general
freertos-variscite which normally includes other config files.

Also remove associated patches since these are integrated now within
varigit/freertos-variscite.

Signed-off-by: Ken Sloat <ken.s@variscite.com>

recipes-bsp/freertos-variscite/freertos-variscite_2.13.x.bb
@@ -5,23 +5,14 @@ include freertos-variscite.inc
 # "Development Tools" section for supported GCC version
 CM_GCC = "10.3-2021.10"
 
-SRCREV = "66cb1c980506524e85364ddadbdaace381c621a6"
+SRCREV = "6a8cb483219ef3d6f9c2ef3dafd094b759eb9f10"
 SRC_URI += " \
     git://github.com/varigit/freertos-variscite.git;protocol=https;branch=${MCUXPRESSO_BRANCH}; \
 "
 
-# The MCUXpresso SDK for the i.MX93 is currently available only from NXP's Beta
-# program. To rebuild from source:
-#  1) Download the MCUXpresso SDK from NXP
-#  2) Add it to your own git repository
-#  3) Change SRC_URI to use your git repository
-#  4) bitbake -c install freertos-variscite
-#  5) The binary is located: tmp/work/imx93_var_som-poky-linux/freertos-variscite/2.13.x-r0/image/lib/firmware/ethosu_firmware
-SRCREV:mx9-nxp-bsp = "48b5b9a8b793dc9f86ad5dbad20a6cf2e4cc36ae"
+SRCREV:mx9-nxp-bsp = "0a30c2c94f77914808b73bd56a815c486eded993"
 SRC_URI:mx9-nxp-bsp = " \
-    git://git@github.com/varigit/freertos-variscite-private;protocol=ssh;branch=${MCUXPRESSO_BRANCH}; \
-    file://0001-mcimx93evk-demo_apps-ethosu_apps_rpmsg-port-to-VAR-S.patch \
-    file://0002-MIMX9352-Correct-multiple-incorect-IOMUXC_-macro-def.patch \
+    git://github.com/varigit/freertos-variscite.git;protocol=https;branch=${MCUXPRESSO_BRANCH}; \
 "
 
 COMPATIBLE_MACHINE = "(mx9-nxp-bsp|imx8mm-var-dart|imx8mn-var-som|imx8mp-var-dart|imx8mq-var-dart)"

conf/machine/variscite.inc
@@ -52,6 +52,11 @@ MACHINE_FIRMWARE:append:mx8-nxp-bsp = " \
 	freertos-variscite \
 	"
 
+# Add Cortex-M33 firmware
+MACHINE_FIRMWARE:append:mx9-nxp-bsp = " \
+	freertos-variscite \
+	"
+
 MACHINE_EXTRA_RDEPENDS += " \
 	gptfdisk \
 	hostapd \

recipes-bsp/freertos-variscite/freertos-variscite.inc
@@ -18,6 +18,7 @@ DEPENDS:append = " cortexm-toolchain-cross (=${CM_GCC})"
 S = "${WORKDIR}/git"
 
 MCUXPRESSO_BRANCH = "mcuxpresso_sdk_${PV}-var01"
+MCUXPRESSO_BRANCH:mx9-nxp-bsp = "mcuxpresso_sdk_${PV}-var02"
 
 INSTALL_SCRIPTS="yes"
 INSTALL_SCRIPTS:mx9-nxp-bsp="no"
@@ -29,7 +30,7 @@ CM_BOARD:mx8mm-nxp-bsp="dart_mx8mm"
 CM_BOARD:mx8mp-nxp-bsp="dart_mx8mp som_mx8mp"
 CM_BOARD:mx8qm-nxp-bsp="som_mx8qm"
 CM_BOARD:mx8qxp-nxp-bsp="som_mx8qx"
-CM_BOARD:mx9-nxp-bsp="mcimx93evk"
+CM_BOARD:mx9-nxp-bsp="som_mx93"
 
 CM_BUILD_TARGETS = " \
     debug \

recipes-bsp/freertos-variscite/freertos-variscite/imx93-var-som/0001-mcimx93evk-demo_apps-ethosu_apps_rpmsg-port-to-VAR-S.patch
@@ -1,69 +1,0 @@
-From 970fdfb32fbede57f4e068a47cdeeb0269b5a47f Mon Sep 17 00:00:00 2001
-From: Nate Drude <nate.d@variscite.com>
-Date: Wed, 11 Jan 2023 16:19:23 -0600
-Subject: [PATCH] mcimx93evk: demo_apps: ethosu_apps_rpmsg: port to
- VAR-SOM-MX93
-
-The following changes are made:
-
-1) Move vring buffers to < 512MB space to allow for SoMs with < 512MB.
-Align with vdev0vring0 and vdev1vring0 in the device tree:
-arch/arm64/boot/dts/freescale/imx93-var-som-symphony.dts
-
-2) Change debug uart from Lpuart7 to Lpuart2. This avoids a conflict with
-the ethernet regulator on the SoM and outputs the M33 messages on
-Symphony J18.3 and J18.5
-
-Signed-off-by: Nate Drude <nate.d@variscite.com>
----
- boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/board.h   | 8 ++++----
- boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/pin_mux.c | 8 ++++----
- 2 files changed, 8 insertions(+), 8 deletions(-)
-
-diff --git a/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/board.h b/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/board.h
-index 12d70d01..4d85f619 100644
---- a/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/board.h
-+++ b/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/board.h
-@@ -23,14 +23,14 @@
-  ******************************************************************************/
- 
- /* The UART to use for debug messages. */
--#define BOARD_DEBUG_UART_INSTANCE   2U
-+#define BOARD_DEBUG_UART_INSTANCE   7U
- #define BOARD_DEBUG_UART_BAUDRATE   115200U
- #define BOARD_DEBUG_UART_TYPE       kSerialPort_Uart
--#define BOARD_DEBUG_UART_CLOCK_ROOT kCLOCK_Root_Lpuart2
-+#define BOARD_DEBUG_UART_CLOCK_ROOT kCLOCK_Root_Lpuart7
- #define BOARD_DEBUG_UART_CLK_FREQ   CLOCK_GetIpFreq(BOARD_DEBUG_UART_CLOCK_ROOT)
- 
--#define VDEV0_VRING_BASE (0xA4000000U)
--#define VDEV1_VRING_BASE (0xA4010000U)
-+#define VDEV0_VRING_BASE (0x87ee0000U)
-+#define VDEV1_VRING_BASE (0x87ef0000U)
- 
- #define LED_INIT()
- #define LED_TOGGLE()
-diff --git a/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/pin_mux.c b/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/pin_mux.c
-index 2d39cf69..ffcef4c9 100644
---- a/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/pin_mux.c
-+++ b/boards/mcimx93evk/demo_apps/ethosu_apps_rpmsg/pin_mux.c
-@@ -51,12 +51,12 @@ BOARD_InitPins:
-  *
-  * END ****************************************************************************************************************/
- void BOARD_InitPins(void) {                                /*!< Function assigned for the core: undefined[cm33] */
--    IOMUXC_SetPinMux(IOMUXC_PAD_UART2_RXD__LPUART2_RX, 0U);
--    IOMUXC_SetPinMux(IOMUXC_PAD_UART2_TXD__LPUART2_TX, 0U);
-+    IOMUXC_SetPinMux(IOMUXC_PAD_GPIO_IO09__LPUART7_RX, 0U);
-+    IOMUXC_SetPinMux(IOMUXC_PAD_GPIO_IO08__LPUART7_TX, 0U);
- 
--    IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_RXD__LPUART2_RX, 
-+    IOMUXC_SetPinConfig(IOMUXC_PAD_GPIO_IO09__LPUART7_RX,
-                         IOMUXC_PAD_PD_MASK);
--    IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_TXD__LPUART2_TX, 
-+    IOMUXC_SetPinConfig(IOMUXC_PAD_GPIO_IO08__LPUART7_TX,
-                         IOMUXC_PAD_DSE(15U));
- }
- 
--- 
-2.39.0
-

commit f81ee791b18f41336cf24392b1b95ebd84f39758

freertos-varsicite: imx8: update to latest
Fixes: e1f8e21ee0e86ed27000b9b549e08e9ba87eb694 by reverting SRCREV for imx8

Signed-off-by: Nate Drude <nate.d@variscite.com>

recipes-bsp/freertos-variscite/freertos-variscite_2.13.x.bb
@@ -5,7 +5,7 @@ include freertos-variscite.inc
 # "Development Tools" section for supported GCC version
 CM_GCC = "10.3-2021.10"
 
-SRCREV = "6a8cb483219ef3d6f9c2ef3dafd094b759eb9f10"
+SRCREV = "66cb1c980506524e85364ddadbdaace381c621a6"
 SRC_URI += " \
     git://github.com/varigit/freertos-variscite.git;protocol=https;branch=${MCUXPRESSO_BRANCH}; \
 "