91661b46402d9e2e9c098ea45b0d7fe028c05743
FrancescoFerraro <francesco.f@variscite.com>
2023-07-23 10:45:32 +0200

    pm-utils: disable stop and start for wifi and bluetooth
    Using DART-6UL with NAND flash, when the BT_EN_GPIO is set to 0 before going
    to suspend, then upon wakeup the UBI filesystem is getting corrupted and is being
    set to read-only.
    
    To avoid this issue, when booting with NAND controller enabled, the WiFi and Bluetooth
    stop/start are disabled for both DART-6UL and VAR-SOM-6UL SOMs.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- recipes-bsp/pm-utils/pm-utils/01-bt.sh
+++ recipes-bsp/pm-utils/pm-utils/01-bt.sh
@@ -2,6 +2,13 @@
 
 [ -x /etc/bluetooth/variscite-bt ] || exit 0
 
+SOC=`cat /sys/bus/soc/devices/soc0/soc_id`
+if [[ $SOC == i.MX6UL* ]] ; then
+	if [[ -d /sys/bus/platform/devices/1806000.nand-controller ]] ; then
+		exit 0
+	fi
+fi
+
 case $1 in
 
 "suspend")
