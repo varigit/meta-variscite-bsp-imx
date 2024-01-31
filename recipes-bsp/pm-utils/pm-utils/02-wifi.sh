1a997d58d862d8ae8ad11d70c542ceef2f2b3d0e
Alexander Herfurtner <alexander.h@variscite.com>
2024-01-31 10:45:20 +0100

    pm-utils-variscite: wifi: remove bashisms
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- recipes-bsp/pm-utils/pm-utils/02-wifi.sh
+++ recipes-bsp/pm-utils/pm-utils/02-wifi.sh
@@ -3,8 +3,8 @@
 [ -x /etc/wifi/variscite-wifi ] || exit 0
 
 SOC=`cat /sys/bus/soc/devices/soc0/soc_id`
-if [[ $SOC == i.MX6UL* ]] ; then
-	if [[ -d /sys/bus/platform/devices/1806000.nand-controller ]] ; then
+if [ "${SOC#i.MX6UL*}" != "${SOC}" ] ; then
+	if [ -d /sys/bus/platform/devices/1806000.nand-controller ] ; then
 		exit 0
 	fi
 fi
