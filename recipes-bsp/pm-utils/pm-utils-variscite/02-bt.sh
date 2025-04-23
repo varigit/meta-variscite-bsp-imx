5fb00ff2adb413f1ff11ced4e675f1b7b801124d
Marius Bagassien <marius.b@variscite.com>
2025-04-23 10:34:03 +0200

    pm-utils-variscite: Fix Bluetooth recovery after suspend/resume
    Aligned with commit 1b391f3, this update ensures that Bluetooth
    functionality is properly restored following a system suspend/resume cycle.
    
    Signed-off-by: Marius Bagassien <marius.b@variscite.com>

--- recipes-bsp/pm-utils/pm-utils-variscite/02-bt.sh
+++ recipes-bsp/pm-utils/pm-utils-variscite/02-bt.sh
@@ -4,7 +4,7 @@
 
 SOC=`cat /sys/bus/soc/devices/soc0/soc_id`
 if [ "${SOC#i.MX6UL*}" != "${SOC}" ] ; then
-	if [ -d /sys/bus/platform/devices/1806000.nand-controller ] ; then
+	if [ -f /proc/device-tree/soc/bus@2100000/mmc@2190000/bcrmf@1/sdio,must-keep-power-in-suspend ] ; then
 		exit 0
 	fi
 fi
