dea282f25c4dbf9f0c902e5dde5c1243ca641927
FrancescoFerraro <francesco.f@variscite.com>
2026-08-19 16:28:49 +0200

    pm-utils-variscite: skip stopping/starting WiFi/BT on suspend/resume
    On 6UL SOMs with a Murata WiFi/BT module the path to test for skipping stopping
    and starting is changed, so add condition accordingly.
    
    This change is an addition to the initial changes made by the following commits:
    - 1b391f3 and 5fb00ff.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- recipes-bsp/pm-utils/pm-utils-variscite/02-bt.sh
+++ recipes-bsp/pm-utils/pm-utils-variscite/02-bt.sh
@@ -4,7 +4,9 @@
 
 SOC=`cat /sys/bus/soc/devices/soc0/soc_id`
 if [ "${SOC#i.MX6UL*}" != "${SOC}" ] ; then
-	if [ -f /proc/device-tree/soc/bus@2100000/mmc@2190000/bcrmf@1/sdio,must-keep-power-in-suspend ] ; then
+	if [ -f /proc/device-tree/soc/bus@2100000/mmc@2190000/bcrmf@1/sdio,must-keep-power-in-suspend ] ||
+	   { [ -d /sys/bus/platform/devices/1806000.nand-controller ] &&
+	     [ -f /proc/device-tree/soc/bus@2100000/mmc@2190000/mmc-pwrseq ]; }; then
 		exit 0
 	fi
 fi
