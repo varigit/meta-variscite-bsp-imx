790ea3872656b0b580d6394861c7c13efd97f38f
Alexander Herfurtner <alexander.h@variscite.com>
2026-09-09 15:11:09 +0200

    pm-utils-variscite: Align code indentation
    Ensure consistent indentation across pm-util scripts.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    (cherry picked from commit 2a064acd52ed51e00b2779c5d3ead0d42ce9fa83)

--- recipes-bsp/pm-utils/pm-utils-variscite/04-usbhub.sh
+++ recipes-bsp/pm-utils/pm-utils-variscite/04-usbhub.sh
@@ -4,11 +4,11 @@ case $1 in
 
 "suspend")
 	# assert USB HUB TUSB8041 on suspend
-        systemctl stop variscite-usbhub
-        ;;
+	systemctl stop variscite-usbhub
+	;;
 "resume")
 	# deassert USB HUB TUSB8041 on resume
 	systemctl start variscite-usbhub
-        ;;
+	;;
 esac
 
